9f325d2d2701cdd42dada9d82c9e70f464295543
Tinh Ngo <tiengtinh@gmail.com>
2017-01-21 19:07:33 +0700

    setup typescript loader

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
+const path = require('path')
 const elixir = require('laravel-elixir')
 require('laravel-elixir-webpack-official')
+
+const assetsPath = path.resolve('./themes/default/assets')
+
+Elixir.webpack.mergeConfig({
+    resolve: {
+        extensions: ['', '.js', '.jsx', '.ts', '.tsx', '.css', '.scss'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                include: assetsPath,
+            },
+        ]
+    }
+});
 
 elixir(function(mix) {
     mix.webpack('app.js', 'themes/default/assets/dist', 'themes/default/assets/scripts');
--- themes/default/assets/scripts/app.js
+++ themes/default/assets/scripts/app.js
@@ -1,1 +1,4 @@
-console.log('hey yeah')
+const bc = 'asdfasdf'
+console.log(`hey yeah ${ bc }`)
+
+import log from './log'
