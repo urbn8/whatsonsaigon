a01c678ad9790206cb63047db33e1630c1290dae
Tinh Ngo <tiengtinh@gmail.com>
2017-01-22 18:44:36 +0700

    create wos plugin with table event_categories

---
+++ plugins/urbn8/wos/Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Urbn8\Wos;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
