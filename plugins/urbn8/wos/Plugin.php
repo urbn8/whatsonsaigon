fbb8252e106b0d7c35d914ee3e7854acaba9ff33
Tinh Ngo <tiengtinh@gmail.com>
2017-02-26 11:24:58 +0700

    test event and log

--- plugins/urbn8/wos/Plugin.php
+++ plugins/urbn8/wos/Plugin.php
@@ -1,9 +1,18 @@
 <?php namespace Urbn8\Wos;
 
+use Event;
+use Log;
 use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
+    public function boot()
+    {
+        Event::listen('eloquent.created: RainLab\User\Models\User', function ($user) {
+            Log::info('User created: '.$user->email);
+        });
+    }
+
     public function registerComponents()
     {
     }
