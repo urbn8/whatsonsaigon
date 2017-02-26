0e3d70cf2e671afb1cb20bf034aca49aa710c214
Tinh Ngo <tiengtinh@gmail.com>
2017-02-26 21:35:09 +0700

    allow attach categories to branches (#8)

---
+++ plugins/urbn8/wos/updates/builder_table_create_urbn8_wos_business_branch_biz_category.php
@@ -0,0 +1,25 @@
+<?php namespace Urbn8\Wos\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableCreateUrbn8WosBusinessBranchBizCategory extends Migration
+{
+    public function up()
+    {
+        Schema::create('urbn8_wos_business_branch_biz_category', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('biz_category_id');
+            $table->integer('business_branch_id');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::dropIfExists('urbn8_wos_business_branch_biz_category');
+    }
+}
