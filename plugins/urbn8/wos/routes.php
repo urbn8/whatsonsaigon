bc6e4b5ba3c146cf5787614bb902aee68a32020f
Tinh Ngo <tiengtinh@gmail.com>
2017-03-24 11:43:23 +0700

    add api organiser

--- plugins/urbn8/wos/routes.php
+++ plugins/urbn8/wos/routes.php
@@ -2,4 +2,5 @@
 
 Route::group(['prefix' => 'api/v1'], function () {
     Route::resource('categories', 'Urbn8\Wos\Http\RestEventCategories');
+    Route::resource('organisers', 'Urbn8\Wos\Http\Organisers');
 });
