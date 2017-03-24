0c4f891a615a6e348074e3f65c75f9c774d7972b
Tinh Ngo <tiengtinh@gmail.com>
2017-03-24 14:22:56 +0700

    organiser relation with user

--- plugins/urbn8/wos/models/Organiser.php
+++ plugins/urbn8/wos/models/Organiser.php
@@ -8,15 +8,41 @@ use Model;
 class Organiser extends Model
 {
     use \October\Rain\Database\Traits\Validation;
+
+    /**
+     * Hard implement the TranslatableModel behavior.
+     */
+    public $implement = ['RainLab.Translate.Behaviors.TranslatableModel'];
     
+    /**
+     * @var array Attributes that support translation, if available.
+     */
+    public $translatable = ['name', 'desc'];
+
+    /**
+     * @var array Generate slugs for these attributes.
+     */
+    protected $slugs = ['slug' => 'name'];
+
+    protected $fillable = ['name', 'desc', 'status'];
+
     /*
      * Validation
      */
     public $rules = [
+        'name' => 'required|between:2,16',
+        'slug' => 'required|unique:urbn8_wos_businesses',
     ];
 
     /**
      * @var string The database table used by the model.
      */
     public $table = 'urbn8_wos_organisers';
+
+    public $belongsToMany = [
+        'users' => [
+            'RainLab\User\Models\User',
+            'table' => 'urbn8_wos_organiser_user',
+        ],
+    ];
 }
