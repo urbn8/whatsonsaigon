a4ba48cad6bb85fecf5f794b3fe3cb61dd611789
Tinh Ngo <tiengtinh@gmail.com>
2017-07-28 10:23:32 +0700

    make react-client-cool

---
+++ react-client-cool/src/components/Organiser.js
@@ -0,0 +1,23 @@
+/*import React, { Component } from 'react'
+import {
+  createFragmentContainer,
+  graphql
+} from 'react-relay'
+
+export class Organiser extends Component {
+  render() {
+    return (
+      <div>
+        { this.props.organiser.name } - { this.props.organiser.slug }
+      </div>
+    )
+  }
+}
+
+export const OrganiserContainer = createFragmentContainer(Organiser, graphql`
+  fragment Organiser_organiser on Organiser {
+    id
+    name
+    slug
+  }
+`)*/
